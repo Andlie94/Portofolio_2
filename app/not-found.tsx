a9e41a22fcce51b245b56b4445dcbc5f5160e6b5
Andlie94 <andlie02174@stud.noroff.no>
2025-12-17 14:59:39 +0100

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,14 +5,14 @@ export default function NotFound() {
     <div className="flex flex-col items-center justify-center h-screen text-center">
       <h1 className="text-6xl font-bold mb-4">404</h1>
       <div className="max-w-md text-xl mb-6 space-y-4">
-      <p >Oops! The page you are looking for does not exist.</p>
-      <p> Maybe it will be found soon.</p>
-      <Link
-        href="/"
-        className="px-6 py-2 bg-[#ffe564] text-[#8627b5] rounded-lg hover:bg-yellow-200 transition font-semibold"
-      >
-        Go back home
-      </Link>
+        <p>Oops! The page you are looking for does not exist.</p>
+        <p> Maybe it will be found soon.</p>
+        <Link
+          href="/"
+          className="px-6 py-2 bg-[#ffe564] text-[#8627b5] rounded-lg hover:bg-yellow-200 transition font-semibold"
+        >
+          Go back home
+        </Link>
       </div>
     </div>
   );
